87df3251ebcb60b690d1029b87c7fa8ff1f37c95
MagnusFrankenberg <frankenberg.magnus@gmail.com>
2023-09-11 17:59:09 +0200

    First commit

---
+++ src/main/java/com/magnus/devopstest01/DevopsTest01Application.java
@@ -0,0 +1,13 @@
+package com.magnus.devopstest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsTest01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/magnus/devopstest01/HelloController.java
@@ -0,0 +1,23 @@
+package com.magnus.devopstest01;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api")
+public class HelloController {
+
+    @GetMapping("greeting/{name}")
+    public String greeting(@PathVariable String name){
+        return "Hello "+name+"!!!";
+    }
+
+    @GetMapping("greeting2")
+    public String greeting2(@RequestParam("name") String name){
+        return "Hello "+name+"!!!";
+    }
+
+    @GetMapping("greeting3")
+    public String greeting3(@RequestParam(name="name", defaultValue= "Guest") String name){
+        return "Hello "+name+"!!!";
+    }
+}
